8ee6117c504766b50205ff04e96e6b2234f4373a
lzpel <18492524+lzpel@users.noreply.github.com>
2025-11-26 11:34:34 +0900

    副業禁止

--- frontend/app/about/about.tsx
+++ frontend/app/about/about.tsx
@@ -11,32 +11,11 @@ export default function About(){
 			所在 神奈川県横浜市鶴見区４－４５－５
 		</Row>
 		<Row>
-			CEO / 代表社員 三角 聡
-		</Row>
-		<Row>
 			設立 2025年11月
 		</Row>
 		<Row>
 			資本金 50万円
 		</Row>
-		<Box
-			sx={{
-				borderLeft: "4px solid",
-				borderColor: "primary.main",
-				pl: 2.5,
-				ml: 1,
-				mt: 5,
-			}}
-		>
-			<img src="/IMG_20240219_183518_copy.jpg" alt="profile" height="200px"/>	
-			<Typography variant="h3">
-				CEO / 代表社員 三角聡
-			</Typography>
-			<Typography>
-				2024年京都大学工学研究科社会基盤工学専攻修了。新卒でソニーグループ株式会社に入社し、AIシステム開発および新規事業開拓に従事。
-				2025年11月サーフィック合同会社設立
-			</Typography>
-		</Box>
 	</ArticleUI>
 }
 function Row(props: {children?: React.ReactNode}){
